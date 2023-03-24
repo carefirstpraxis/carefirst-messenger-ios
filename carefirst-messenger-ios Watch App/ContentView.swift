18e71c2550f44c11676705e77e7939cc57cd6430
Nick Sophinos <nick@wdeanmedical.com>
2023-03-24 13:12:09 -0400

    project creation

---
+++ carefirst-messenger-ios Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  carefirst-messenger-ios Watch App
+//
+//  Created by Nick Sophinos on 3/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ carefirst-messenger-ios Watch App/carefirst_messenger_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  carefirst_messenger_iosApp.swift
+//  carefirst-messenger-ios Watch App
+//
+//  Created by Nick Sophinos on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct carefirst_messenger_ios_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
